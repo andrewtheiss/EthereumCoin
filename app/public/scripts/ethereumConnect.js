cb9a1507398564bfea6d39f9ee7a102ae66b32f8
Andrew Theiss <andrew.theiss@gmail.com>
2021-09-18 16:23:03 -0700

    Add network and some mobile css

--- app/public/scripts/ethereumConnect.js
+++ app/public/scripts/ethereumConnect.js
@@ -1,3 +1,14 @@
+let NETWORK_ENV = 'DEV'; // 'PROD' is
+
+/**
+ *
+ *  Check if on polygon network or testnet and
+ */
+function checkAndNotifyOfWrongNetwork() {
+  // Check network ID's : Move this to a config file
+
+}
+
 /**
  * showWalletConnected
  *    will update all UI elements given the connected account
@@ -11,8 +22,10 @@ function showWalletConnected() {
   const el2 = document.querySelector('#wallet-connect-button');
   if (!el2.classList.contains("hidden")) {
     el2.classList.add("hidden");
-
   }
+
+  // Check and notify of wrong network
+  checkAndNotifyOfWrongNetwork();
 
 // HWCoin.currentAccount
   const link = document.getElementById('link-to-etherscan');
@@ -46,6 +59,7 @@ function showWalletConnected() {
  *  Create button event to connect MetaMask account
  */
 document.getElementById("wallet-connect-button").onclick = async function() {
+  swtichToPolygonNetwork();
   try {
 
     // Step 1 is giving metamask permission to access the website
@@ -114,3 +128,74 @@ ethereum.on('chainChanged', (chainId) => {
   // We recommend reloading the page unless you have good reason not to.
   window.location.reload();
 });
+
+
+
+async function swtichToPolygonNetwork() {
+  // Swtich Eth chain to desired mainnet
+  try {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x89' }],
+    });
+  } catch (switchError) {
+    // This error code indicates that the chain has not been added to MetaMask.
+    if (switchError.code === 4902) {
+
+      //
+    let goerli = {
+        "id": 1,
+        "jsonrpc": "2.0",
+        "method": "wallet_addEthereumChain",
+        "params": [
+          {
+            "chainId": "0x5",
+            "chainName": "Goerli",
+            "rpcUrls": ["https://goerli.infura.io/v3/INSERT_API_KEY_HERE"],
+            "nativeCurrency": {
+              "name": "Goerli ETH",
+              "symbol": "gorETH",
+              "decimals": 18
+            },
+            "blockExplorerUrls": ["https://goerli.etherscan.io"]
+          }
+        ]
+      };
+
+      let matic = {
+        method: 'wallet_addEthereumChain',
+        params: [{
+          // MUST specify the integer ID of the chain as a hexadecimal string, per the eth_chainId Ethereum RPC method.
+          // The wallet SHOULD compare the specified chainId value with the eth_chainId return value from the endpoint.
+          // If these values are not identical, the wallet MUST reject the request.
+          chainId: '0x89',
+
+          // If provided, MUST specify one or more URLs pointing to RPC endpoints that can be used to communicate with the chain.
+          rpcUrls: ['https://rpc-mainnet.matic.network'],
+
+          // Seriously you need a comment for this?
+          symbol: 'MATIC',
+
+          // If provided, MUST specify one or more URLs pointing to block explorer web sites for the chain.
+          blockExplorerUrls : ['https://polygonscan.com/'],
+
+          nativeCurrency: {
+            name: "MATIC",
+            symbol: "MATIC",
+            decimals: 18
+          },
+
+          // If provided, MUST specify a human-readable name for the chain.
+          chainName : 'Polygon'
+         }]
+      };
+      try {
+        await ethereum.request(matic);
+      } catch (addError) {
+        // handle "add" error
+      }
+    }
+    // handle other "switch" errors
+  }
+
+}
