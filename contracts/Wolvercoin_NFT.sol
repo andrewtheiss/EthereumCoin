83447113d5664bd31042bf88563ab5ab3d0e41ec
Andrew Theiss <andrew.theiss@gmail.com>
2021-11-09 12:24:35 -0800

    Add updated Wolvercoin NFT code with owner, details, and Extra Credit hidden

--- contracts/Wolvercoin_NFT.sol
+++ contracts/Wolvercoin_NFT.sol
@@ -1,47 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "./includes/ERC721.sol";
-import "./WolverAuctions.sol";
+import "./includes/Owner.sol";
+import "./includes/ERC721Enumerable.sol";
 
-contract Wolvercoin_NFT is ERC721 {
+contract Wolvercoin_NFT is Owner, ERC721Enumerable{
+  
+    // We want to allow students to help mint objects but then stop 
+    // giving them permission after sometime.  So we set and salt a 
+    // password to give access
+    uint password;
+  
+    constructor(string memory name, string memory symbol, string memory _password) ERC721(name, symbol) {
+        password = _saltPassword(_password);
+    }
+
     
-    // Add Auction Capability
-    WinAuction[] winAuctions;
-    
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
-
-    function baseURI() public view returns (string memory) {
-        return _baseURI();
-    }
-
-    function exists(uint256 tokenId) public view returns (bool) {
-        return _exists(tokenId);
-    }
-
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
-    }
-
-    function safeMint(address to, uint256 tokenId) public {
+    function safeMintAsOwner(address to, uint256 tokenId) public isOwner {
         _safeMint(to, tokenId);
     }
-
-    function safeMint(
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) public {
-        _safeMint(to, tokenId, _data);
+    
+    function safeMintToContractAsOwner(uint256 tokenId) public isOwner {
+        _safeMint(address(this), tokenId);
+    }
+    
+        
+    // Modifiers can take inputs. This modifier checks that the
+    // user used a correct password
+    modifier usesPassword(string memory _password) {
+        require(password == _saltPassword(_password), "Incorrect Password");
+        _;
     }
 
-    function burn(uint256 tokenId) public {
-        _burn(tokenId);
+    // Take the password input and salts it 
+    function _saltPassword(string memory _password) private pure returns (uint) {
+        uint pwd = uint(keccak256(abi.encode(_password)));
+        uint salt = uint(keccak256(abi.encode("Email 'OMGWTFBBQ' 1st 2Mr.Theiss4exCred")));
+        return (pwd + salt);
     }
-    /*
-    function createDutchAuction(uint256 _startingPrice, uint256 _priceDeductionRate, address _nft, uint256 _nftId) public {
-        DutchAuction d = new DutchAuction(_startingPrice, _priceDeductionRate, _nft, _nftId);
-        dutchAuctions.push(d);
+    
+    // Sets a new password, if we are the owner only!
+    function setPassword(string memory _password) public isOwner returns (bool) {
+        password = _saltPassword(_password);
+        return true;
     }
-    */
 }
+
